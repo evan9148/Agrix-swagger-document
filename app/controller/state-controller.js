923a3e957a1c7b6a337e401ec6758e3236dca4fa
evan9148 <you@example.com>
2022-10-17 13:37:29 +0530

    states and district entries were added and schema has been updated

--- app/controller/state-controller.js
+++ app/controller/state-controller.js
@@ -2,51 +2,6 @@ const db=require("../models");
 const State=db.state;
 
 
-// old state 
-// // All state
-// exports.getState = (req,res) =>{
-//     const state=req.query.state;
-//     State.find(state)
-//         .then(data =>{
-//             if(!data)
-//                 res.status(404).send({messsage: "Not found State with id " + state});
-//             else res.send(data);
-//         })
-//         .catch(error =>{
-//             res
-//                 .status(500)
-//                 .send({message: "Error retrieving State with id=" + state})
-//         });
-// }
-
-// // Add state
-// exports.addState = (req,res) =>{
-//     const id=req.body._id;
-//     const state=new State({
-//         state:req.body.state,
-//         id:req.body.id
-//     });
-
-//     state
-//         .save(state)
-//         .then(data=>{
-//             res.send(data);
-//         })
-//         .catch(error =>{
-//             res.status(500).send({
-//                 messsage:
-//                     error.messsage || "some error occurred while creating the state"
-//             });
-//         });
-// }
-
-
-
-
-
-
-//komal
-
 // Add state
 exports.addState = (req,res) =>{
     const id=req.body._id;
